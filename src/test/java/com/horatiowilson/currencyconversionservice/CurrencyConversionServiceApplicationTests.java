36a22a0f099754aa31e9df476cdd4d797581690e
Horatio Wilson <horatio.wilson@teamrockstars.nl>
2023-11-16 18:22:48 +0100

    Initial spring files

---
+++ src/test/java/com/horatiowilson/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horatiowilson.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
